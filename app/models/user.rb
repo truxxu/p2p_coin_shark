a29536dd87d613d9b9ec4a6d01fcf11f3055b9ec
Daniel Trujillo <ganimedes@firemail.cc>
2019-03-04 09:13:37 +0100

    wrote test for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :loans
+  has_many :requests
+  # has_many :requests_as_owner, thorugh: :loans, source: :requests
+
+  ID_TYPE = ["Passport", "Driver's License", "Birth Certificate", "Permanent Resident Card"]
+
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :id_type, presence: true, inclusion: { in: ID_TYPE }
+  validates :id_number, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  # validates :photo, presence: true
+
   mount_uploader :photo, PhotoUploader
 
   def full_address
